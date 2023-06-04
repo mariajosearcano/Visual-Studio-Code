032b504cef642a770aea2c21a4cda1cb459b16cc
mariajosearcano <mariajosearcano@outlook.com>
2023-06-04 14:31:51 -0500

    obvio

---
+++ javascript-curso/app.js
@@ -0,0 +1,77 @@
+/*
+//document.write("<h1>Hello World</h1>");
+
+// tipos de datos
+"Hello World" // string
+'Hello World' // string
+    ' I am'
+    'I' am'
+
+1000000 // number
+-2.3
+
+true // boolean
+false
+
+['joe', 'ryan', 'martha'] // array
+[1, 2, 3]
+[true, false, true, false]
+
+'ryan' // object
+70.4
+14
+true8
+{
+    //clave    //valor
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+    //"friends": ['','','']
+}
+{
+    //el error sale por que no se esta escribiendo codigo valido
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": false,
+}
+*/
+
+//console.log('this is a string');
+//console.log(4.3);
+//console.log(true);
+//console.log([1, 2, 3, 4]);
+//console.log({"username": "ryan", "score": 70.4});
+
+/*var nameuser = "john";
+let lastname = "carter";
+
+nameuser = 'pepe';
+
+const PI = 3.1415;
+//PI = 100; sale un error por que es una constante
+//const id = 'user1';
+
+console.log(PI);
+
+//1username = 'fazt'; error
+//@username = 'fazt';
+
+// camelcase (joroba de camello)
+let nombreDePersona = 'gordon'; //convencion de nombre de varibles
+//user_name es otra manera*/
+
+//suma de numeros y caracteres es lo mismo que ne java
+//el if tambien es igual, pero aqui TAMBIEN existe ===
+//el switch es igualito
+//los ciclos tambiem, existe c++. Si se queda en un ciclo la pagina no terminara de cargar nunca
+//acceder a un dato especifico del array es igual array[0], tambien exite array.length que inicio tambien de 1
+//for igualito
+
+function greeting(name) { //funciones
+    console.log(name); //undefined si no tiene ningun tipo de datos
+    console.log('Hello');
+}
+
+greeting('Marcus');
