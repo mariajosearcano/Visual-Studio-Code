be58ecf186bd28c3e8d3d7eb8fbd24d737d5d638
mariajosearcano <mariajosearcano@outlook.com>
2023-06-11 06:25:28 -0500

    funcion fetch

--- json-course/index.js
+++ json-course/index.js
@@ -52,12 +52,24 @@ for (let i = 0; i < friends.length; i++) {
     output = output + `<li>${friends[i].name} - ${friends[i].nickname}</li>`
 }
 
+//fetch: es una función en JavaScript que se utiliza para realizar solicitudes de red y obtener recursos de un servidor
 fetch('/user.json')
     .then(response => response.json())
     .then(data => {
         //console.log(data)
         document.getElementById('people').innerHTML = `<h1>${data.name} ${data.lastname}</h1>`
     })
+/*
+El código que has compartido utiliza la función fetch para realizar una solicitud a un archivo JSON llamado "user.json" en el servidor.
+
+A continuación, se encadena una serie de promesas (then) para manejar la respuesta de la solicitud.
+
+En el primer then, response.json() se utiliza para convertir la respuesta de la solicitud en formato JSON en un objeto JavaScript. Esto se realiza llamando al método json() en el objeto response devuelto por la función fetch.
+
+En el segundo then, data representa los datos obtenidos de la respuesta convertida en JSON. En este caso, el código asume que el archivo JSON contiene propiedades name y lastname. Luego, se utiliza document.getElementById('people') para obtener el elemento HTML con el id "people" y se establece su contenido utilizando innerHTML. El contenido del elemento HTML se establece como un encabezado (<h1>) que muestra el nombre y el apellido obtenidos de data.
+
+En resumen, este código realiza una solicitud a un archivo JSON, obtiene los datos de la respuesta y los muestra en un elemento HTML en la página.
+*/
 
 //console.log(JSON.stringify(user))
 
@@ -87,3 +99,16 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         document.getElementById('posts').innerHTML = postsElements
 
     })
+/*
+El código que has compartido utiliza la función fetch para realizar una solicitud GET a la URL "https://jsonplaceholder.typicode.com/posts" y obtener una lista de publicaciones (posts) en formato JSON.
+
+Luego, se encadenan promesas (then) para manejar la respuesta de la solicitud.
+
+En el primer then, response.json() se utiliza para convertir la respuesta en formato JSON en un objeto JavaScript. Esto se realiza llamando al método json() en el objeto response devuelto por la función fetch. El resultado se pasa al siguiente then como el parámetro data.
+
+En el segundo then, se itera sobre los elementos en data usando un bucle for. Para cada elemento, se crea una cadena HTML <li> que muestra el userId y el título de la publicación. Estos elementos se concatenan a la variable postsElements.
+
+Finalmente, se utiliza document.getElementById('posts') para obtener el elemento HTML con el id "posts" y se establece su contenido utilizando innerHTML. El contenido del elemento se establece como la cadena HTML generada en el bucle for, que muestra la lista de publicaciones.
+
+En resumen, este código realiza una solicitud GET a una API de prueba, obtiene una lista de publicaciones en formato JSON y muestra los userId y títulos de las publicaciones en una lista en la página HTML.
+*/
